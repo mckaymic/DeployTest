709b410ca27f6c4be1108d4ddb4ca5869464a64d
Michael McKay <mckaymic@us.ibm.com>
2015-09-02 19:27:11 -0400

    Updates to NewRelic config

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NEW_RELIC_LICENSE_KEY && process.env.NEW_RELIC_LICENSE_KEY !== "") {
+  require('newrelic');
+}
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
